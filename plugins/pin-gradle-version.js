976f79c0622fffa6d0a275f2ac2515625916066a
LakshanyaAS <reachlakshanya@gmail.com>
2025-10-28 13:52:37 +0530

    Fix gradle-plugin type mismatch

--- plugins/pin-gradle-version.js
+++ plugins/pin-gradle-version.js
@@ -4,7 +4,7 @@ module.exports = function withPinnedGradle(config) {
   return withProjectBuildGradle(config, (config) => {
     config.modResults.contents = config.modResults.contents.replace(
       /distributionUrl=.*\n/,
-      'distributionUrl=https\\://services.gradle.org/distributions/gradle-8.5-bin.zip\n'
+      'distributionUrl=https\\://services.gradle.org/distributions/gradle-7.6.3-bin.zip\n'
     );
     return config;
   });
